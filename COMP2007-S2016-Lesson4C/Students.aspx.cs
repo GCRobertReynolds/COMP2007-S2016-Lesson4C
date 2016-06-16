fb090c49a1f7ecb17707c04c2f049907f6ae0887
Robert <gcrobertreynolds@gmail.com>
2016-06-16 18:02:52 -0400

    added gridview to students page and connected to database

---
+++ COMP2007-S2016-Lesson4C/Students.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using statements that are required to connect to Entity Framework Database
+using COMP2007_S2016_Lesson4C.Models;
+using System.Web.ModelBinding;
+
+namespace COMP2007_S2016_Lesson4C
+{
+    public partial class Students : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if loading the page for the first time, populate the student grid
+            if(!IsPostBack)
+            {
+                //Get the student data
+                this.GetStudents();
+            }
+        }
+
+        /**
+         * <summary>
+         * This method gets the student data from the database
+         * </summary>
+         * 
+         * @method GetStudents
+         * @returns {void}
+         */
+        protected void GetStudents()
+        {
+            //Connect to Entity Framework
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //Query the Students Table using Entity Framework and LINQ
+                var Students = (from allStudents in db.Students
+                                select allStudents);
+
+                //Bind the results to the GridView
+                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataBind();
+            }
+        }
+    }
+}
