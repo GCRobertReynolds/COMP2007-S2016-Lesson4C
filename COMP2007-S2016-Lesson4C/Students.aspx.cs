5eb42c56a21992f034535216ef5f2c9b610bafe8
Robert <gcrobertreynolds@gmail.com>
2016-06-18 12:04:08 -0400

    added delete functionality to Students page

--- COMP2007-S2016-Lesson4C/Students.aspx.cs
+++ COMP2007-S2016-Lesson4C/Students.aspx.cs
@@ -44,5 +44,63 @@ namespace COMP2007_S2016_Lesson4C
                 StudentsGridView.DataBind();
             }
         }
+
+        /**
+         * <summary>
+         * This event handler deletes a student from the database using entity framework
+         * </summary>
+         * 
+         * @method StudentGridView_RowDeleting
+         * @param {object} sender
+         * @param {GridviewDeleteEventArgs} e
+         * @returns {void}
+         * 
+         */
+        protected void StudentsGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //Store which row was selected
+            int selectedRow = e.RowIndex;
+
+            //Get the selected StudentID using the grids DataKey collection
+            int StudentID = Convert.ToInt32(StudentsGridView.DataKeys[selectedRow].Values["StudentID"]);
+
+            //Use Entity Framework to find the selected student in the DB and remove it
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //Create object of the student class and store the query string inside of it
+                Student deletedStudent = (from studentRecords in db.Students
+                                          where studentRecords.StudentID == StudentID
+                                          select studentRecords).FirstOrDefault();
+
+                //Remove the selected student from the database
+                db.Students.Remove(deletedStudent);
+
+                //Save changes to database
+                db.SaveChanges();
+
+                //Refresh the gridview
+                this.GetStudents();
+            }
+        }
+
+        /**
+         * <summary>
+         * This event handler allows pagination to occur for the Students page
+         * </summary>
+         * 
+         * @method StudentsGridView_PageIndexChanging
+         * @param {object} sender
+         * @param {GridViewPageEventArgs} e
+         * returns {void}
+         * 
+         */
+        protected void StudentsGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            //Set the new page number
+            StudentsGridView.PageIndex = e.NewPageIndex;
+
+            //Refresh the grid
+            this.GetStudents();
+        }
     }
 }
